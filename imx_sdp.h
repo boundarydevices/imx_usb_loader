28837c95fb61806ac6791cd32b8ae1c91c29a768
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-06-02 22:48:18 -0700

    use a nicely named struct to hold the SDP command

--- imx_sdp.h
+++ imx_sdp.h
@@ -77,6 +77,24 @@ struct sdp_dev {
 	void *priv;
 };
 
+#define SDP_READ_REG     0x0101
+#define SDP_WRITE_REG    0x0202
+#define SDP_WRITE_FILE   0x0404
+#define SDP_ERROR_STATUS 0x0505
+#define SDP_WRITE_DCD    0x0a0a
+#define SDP_JUMP_ADDRESS 0x0b0b
+
+#pragma pack (1)
+struct sdp_command {
+	uint16_t cmd;
+	uint32_t addr;
+	uint8_t format;
+	uint32_t cnt;
+	uint32_t data;
+	uint8_t rsvd;
+};
+#pragma pack ()
+
 int get_val(const char** pp, int base);
 const unsigned char *move_string(unsigned char *dest, const unsigned char *src, unsigned cnt);
 void dump_bytes(unsigned char *src, unsigned cnt, unsigned addr);
